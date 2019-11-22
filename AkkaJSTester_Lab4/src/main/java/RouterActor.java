7a3e186663388ff7e2b275b02cdd226b1e925cd8
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-23 02:07:59 +0300

    Scripted auto-commit on change (2019-11-23 02:07:59) by gitwatch.sh

--- AkkaJSTester_Lab4/src/main/java/RouterActor.java
+++ AkkaJSTester_Lab4/src/main/java/RouterActor.java
@@ -11,7 +11,7 @@ public class RouterActor extends AbstractActor {
     public AbstractActor.Receive createReceive() {
         return receiveBuilder()
                 .match(StoreMessage.class), sm ->StoreActor.tell(
-                        sm
+                        sm, self
         )
 
 
