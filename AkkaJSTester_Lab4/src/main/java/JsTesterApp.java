ae76e654935b5ce118585fd48626e9d1524d9347
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-22 18:23:40 +0300

    Scripted auto-commit on change (2019-11-22 18:23:40) by gitwatch.sh

--- AkkaJSTester_Lab4/src/main/java/JsTesterApp.java
+++ AkkaJSTester_Lab4/src/main/java/JsTesterApp.java
@@ -6,9 +6,10 @@ import akka.actor.Props;
 import java.io.IOException;
 
 public class JsTesterApp {
-    public static void main(String[] args ) throws IOException
-    ActorSystem system = ActorSystem.create("test");
-    ActorRef storeActor = system.actorOf(
-            Props.create(StoreActor.class)
-    );
+    public static void main(String[] args ) throws IOException {
+        ActorSystem system = ActorSystem.create("test");
+        ActorRef storeActor = system.actorOf(
+                Props.create(StoreActor.class)
+        );
+    }
 }
