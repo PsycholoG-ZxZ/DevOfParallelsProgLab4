7e1ffa08429617542e4b3f0f92ac483a70c70370
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-22 18:36:35 +0300

    Scripted auto-commit on change (2019-11-22 18:36:35) by gitwatch.sh

--- AkkaJSTester_Lab4/src/main/java/StoreActor.java
+++ AkkaJSTester_Lab4/src/main/java/StoreActor.java
@@ -12,7 +12,7 @@ public class StoreActor extends AbstractActor {
                     store.put(m.getKey(), m.getValue());
                     System.out.println("receive mess!" + m.toString());
                 })
-                .match(GetMess(req.get))
+                .match(GetMess(req.getKey()))
 
     }
 }
