8768d32e3a580963010d9f42221f4be5f9643b2c
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-23 02:32:35 +0300

    Scripted auto-commit on change (2019-11-23 02:32:35) by gitwatch.sh

--- AkkaJSTester_Lab4/src/main/java/DataTest.java
+++ AkkaJSTester_Lab4/src/main/java/DataTest.java
@@ -7,5 +7,7 @@ public class DataTest {
     private String exResult;
     public Object[] params;
 
-    DataTest(@JsonProperty("testName") String testName, @JsonProperty("expectedResult") String exResult, @JsonProperty("params") String params)
+    DataTest(@JsonProperty("testName") String testName, @JsonProperty("expectedResult") String exResult, @JsonProperty("params") String params){
+        
+    }
 }
