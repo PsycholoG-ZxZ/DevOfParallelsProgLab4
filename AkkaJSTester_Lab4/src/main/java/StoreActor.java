f7c1b02efe7b61380a89664d01d959efb1bd1964
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-22 20:34:25 +0300

    Scripted auto-commit on change (2019-11-22 20:34:25) by gitwatch.sh

--- AkkaJSTester_Lab4/src/main/java/StoreActor.java
+++ AkkaJSTester_Lab4/src/main/java/StoreActor.java
@@ -1,11 +1,12 @@
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class StoreActor extends AbstractActor {
-    private Map<String, String> store = new HashMap<>();
+    private Map<String, ArrayList<String>> store = new HashMap<>();
     @Override
     public Receive createReceive(){
         return ReceiveBuilder.create()
