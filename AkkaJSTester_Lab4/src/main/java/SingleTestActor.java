4422bd66aa610ac224fb0a58be1d9ecf660e9a0d
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-22 20:47:05 +0300

    Scripted auto-commit on change (2019-11-22 20:47:05) by gitwatch.sh

--- AkkaJSTester_Lab4/src/main/java/SingleTestActor.java
+++ AkkaJSTester_Lab4/src/main/java/SingleTestActor.java
@@ -2,6 +2,15 @@ import akka.actor.AbstractActor;
 
 public class SingleTestActor extends AbstractActor{
     @Override
-    public Receive createReceive(){
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(StoreMessage.class, m -> {
+                    store.put(m.getKey(), m.getValue());
+                    System.out.println("receive message! "+m.toString());
+                })
+                .match(GetMessage.class, req -> sender().tell(
+                        new StoreMessage(req.getKey(), store.get(req.getKey())), self())
+                ).build();
+    }
 
 }
