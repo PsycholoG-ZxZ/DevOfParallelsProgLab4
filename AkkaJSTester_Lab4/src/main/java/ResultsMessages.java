b3c6d1e21cfb54bb8edaa68c482c7dd9b68d07d0
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-22 19:28:20 +0300

    Scripted auto-commit on change (2019-11-22 19:28:20) by gitwatch.sh

--- AkkaJSTester_Lab4/src/main/java/ResultsMessages.java
+++ AkkaJSTester_Lab4/src/main/java/ResultsMessages.java
@@ -1,4 +1,13 @@
 public class ResultsMessages {
     private String value, packageId;
 
+    
+
+    public String getValue(){
+        return value;
+    }
+
+    public String getPackageId(){
+        return packageId;
+    }
 }
