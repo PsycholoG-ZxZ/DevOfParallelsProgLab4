202771b321322bb61da5a7573f5abfdf2b15bd21
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-22 18:20:13 +0300

    Scripted auto-commit on change (2019-11-22 18:20:12) by gitwatch.sh

--- AkkaJSTester_Lab4/src/main/java/JsTesterApp.java
+++ AkkaJSTester_Lab4/src/main/java/JsTesterApp.java
@@ -1,7 +1,11 @@
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 
 public class JsTesterApp {
     ActorSystem system = ActorSystem.create("test");
-    
+    ActorRef storeActor = system.actorOf(
+            Props.create(StoreActor.class)
+    );
 }
