42c339fdd3b68acc28b65d6987d97f4c404b50b5
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-22 18:28:40 +0300

    Scripted auto-commit on change (2019-11-22 18:28:40) by gitwatch.sh

--- AkkaJSTester_Lab4/src/main/java/StoreActor.java
+++ AkkaJSTester_Lab4/src/main/java/StoreActor.java
@@ -6,5 +6,7 @@ import java.util.Map;
 public class StoreActor extends AbstractActor {
     private Map<String, String> store = new HashMap<>();
     @Override
-    public Receive createReceive
+    public Receive createReceive(){
+        
+    }
 }
