2c3b7f1b5723c0ad8ad734952b720e07f984861d
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-23 02:18:03 +0300

    Scripted auto-commit on change (2019-11-23 02:18:03) by gitwatch.sh

--- AkkaJSTester_Lab4/src/main/java/JsRequestMessage.java
+++ AkkaJSTester_Lab4/src/main/java/JsRequestMessage.java
@@ -1,7 +1,9 @@
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public class JsRequestMessage {
     public String packId;
     public String jsScript;
     public String functionName;
     public Object[] tests;
-    @Json
+    @JsonCreator
 }
