5ffa907b2c8154dea162ec8eaa7e10f20fa5affd
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-23 02:29:08 +0300

    Scripted auto-commit on change (2019-11-23 02:29:08) by gitwatch.sh

--- AkkaJSTester_Lab4/src/main/java/DataTest.java
+++ AkkaJSTester_Lab4/src/main/java/DataTest.java
@@ -1,7 +1,9 @@
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public class DataTest {
     private String Name;
     private String Result;
     public Object[] values;
 
-    DataTest
+    DataTest(@JsonCreator)
 }
