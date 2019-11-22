99cd0bba39569d9adc40cf2faad3f97dba523e8f
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-23 01:11:07 +0300

    Scripted auto-commit on change (2019-11-23 01:11:07) by gitwatch.sh

--- AkkaJSTester_Lab4/src/main/java/RouterActor.java
+++ AkkaJSTester_Lab4/src/main/java/RouterActor.java
@@ -6,8 +6,8 @@ public class RouterActor extends AbstractActor {
     @Override
     public AbstractActor.Receive createReceive() {
         private ActorRef StoreActor = getContext().actorOf(
-                Props.create(StoreActor::new)
-        )
+                Props.create(StoreActor.class)
+        );
 
 
     }
