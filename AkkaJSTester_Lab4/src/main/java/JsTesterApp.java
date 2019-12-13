0b1526458e4b5911bfd12837ea33cbcc44592885
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-14 00:54:21 +0300

    Scripted auto-commit on change (2019-12-14 00:54:21) by gitwatch.sh

--- AkkaJSTester_Lab4/src/main/java/JsTesterApp.java
+++ AkkaJSTester_Lab4/src/main/java/JsTesterApp.java
@@ -1,9 +1,13 @@
+import akka.NotUsed;
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 import scala.App;
 
 import java.io.IOException;
@@ -18,6 +22,7 @@ public class JsTesterApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         JsTesterApp app = new JsTesterApp();
-        
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> 
     }
 }
