195c979c38970c3a872dc4b3c0790b324ae2b8e1
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-14 01:01:00 +0300

    Scripted auto-commit on change (2019-12-14 01:00:59) by gitwatch.sh

--- AkkaJSTester_Lab4/src/main/java/JsTesterApp.java
+++ AkkaJSTester_Lab4/src/main/java/JsTesterApp.java
@@ -6,11 +6,15 @@ import akka.actor.Props;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
-import scala.App;
-
 import java.io.IOException;
+import java.util.concurrent.Future;
+import java.util.regex.Pattern;
+
+import static akka.http.javadsl.server.Directives.get;
+import static akka.http.javadsl.server.Directives.route;
 
 public class JsTesterApp {
     public static void main(String[] args ) throws IOException {
@@ -23,6 +27,18 @@ public class JsTesterApp {
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         JsTesterApp app = new JsTesterApp();
 
-        final Flow<HttpRequest, HttpResponse, NotUsed> 
+        final Flow<HttpRequest, HttpResponse, NotUsed> directFlow = app
+    }
+
+    public Route routeCreater(){
+        return route(
+                get(
+                        () -> parameter("packageId", packId ->{
+                            Future<Object> futureRes = Pattern.ask(
+                                    RouterActor, 
+                            )
+                        })
+                )
+        )
     }
 }
