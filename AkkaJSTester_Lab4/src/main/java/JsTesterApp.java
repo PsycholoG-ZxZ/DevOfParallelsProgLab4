c5667961cab28e66d506f1e7f24f5582e690d299
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-14 02:40:09 +0300

    Scripted auto-commit on change (2019-12-14 02:40:09) by gitwatch.sh

--- AkkaJSTester_Lab4/src/main/java/JsTesterApp.java
+++ AkkaJSTester_Lab4/src/main/java/JsTesterApp.java
@@ -3,7 +3,9 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
@@ -11,6 +13,7 @@ import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 import akka.pattern.Patterns;
 import scala.concurrent.Future;
@@ -31,8 +34,10 @@ public class JsTesterApp {
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         JsTesterApp app = new JsTesterApp();
 
-        final Flow<HttpRequest, HttpResponse, NotUsed> directFlow = app
-                
+        final Flow<HttpRequest, HttpResponse, NotUsed> directFlow = app.routeCreater(routerActor).flow(system, materializer);
+        final CompletionStage<ServerBinding> bind = http.bindAndHandle( directFlow, ConnectHttp.toHost("localhost", 8008),
+                materializer);
+        System.out.println("Server online at localhost");
     }
 
     public Route routeCreater(ActorRef routerActor){
